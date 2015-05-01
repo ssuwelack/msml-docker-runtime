7edf48e964070e1263d08929fef3255c6ca8fdc2
Stefan Suwelack <suwelack@kit.edu>
2015-05-01 15:08:35 +0200

    Start scripts for msml_sofa dded

---
+++ run_msml_sofa.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if test $# -lt 1; then
+	# Get the latest msml_docker build
+	# and start with an interactive terminal enabled
+	args="-i -t $(docker images | grep ^ssuwelack/msml_sofa | head -n 1 | awk '{ print $1":"$2 }')"
+else
+        # Use this script with derived images, and pass your 'docker run' args
+	args="$@"
+fi
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run \
+	-v $XSOCK:$XSOCK:rw \
+	-v $XAUTH:$XAUTH:rw \
+	--device=/dev/dri/card0:/dev/dri/card0 \
+	-e DISPLAY=$DISPLAY \
+	-e XAUTHORITY=$XAUTH \
+	$args 
---
+++ start_ssh_msml_sofa.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if test $# -lt 1; then
+	# Get the latest msml_docker build
+	# and start with an interactive terminal enabled
+	args="-d $(docker images | grep ^ssuwelack/msml_sofa | head -n 1 | awk '{ print $1":"$2 }') /root/start_ssh.sh"
+else
+        # Use this script with derived images, and pass your 'docker run' args
+	args="$@"
+fi
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run \
+  -p 127.0.0.1:22000:22 \
+  --name msml_sofa \
+	$args 
+
+
