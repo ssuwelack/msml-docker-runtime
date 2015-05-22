a526d315be1de2a50a3bc1467fbf35ab4359bbec
Stefan Suwelack <suwelack@kit.edu>
2015-05-22 10:36:06 +0200

    Added runtime for dev container

--- start_ssh_msml_dev.sh
+++ start_ssh_msml_dev.sh
@@ -2,21 +2,20 @@
 if test $# -lt 1; then
 	# Get the latest msml_docker build
 	# and start with an interactive terminal enabled
-	args="-i -t $(docker images | grep ^ssuwelack/msml_sofa | head -n 1 | awk '{ print $1":"$2 }')"
+	args="-d $(docker images | grep ^ssuwelack/msml_dev | head -n 1 | awk '{ print $1":"$2 }') /root/start_ssh.sh"
 else
         # Use this script with derived images, and pass your 'docker run' args
 	args="$@"
 fi
 
 XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth-suwelack
+XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run \
-	-v $XSOCK:$XSOCK:rw \
-	-v $XAUTH:$XAUTH:rw \
-	--device=/dev/dri/card0:/dev/dri/card0 \
-	-e DISPLAY=$DISPLAY \
-	-e XAUTHORITY=$XAUTH \
+  -p 127.0.0.1:22000:22 \
+  --name msml_sofa \
 	$args 
+
+
